f43eb8c613f0730095664784655c3d5716e9da10
bigriceman0128 <112298940+bigriceman0128@users.noreply.github.com>
2023-06-07 23:24:14 -0600

    Create coinflip.py
    simple project for flipping a coin September 2022

---
+++ coinflip.py
@@ -0,0 +1,28 @@
+##COIN FLIP
+
+from logging import exception
+from random import randint
+
+def coin_flipper(user_input):
+    for turns in range(0,user_input):
+        flipped = randint(1,2)
+
+        if flipped == 1:
+            print('heads')
+        else:
+            print('tails')
+
+
+def inputter():
+    while True:
+        try:
+            ammount = int(input('How many times would you like to flip coin?: '))
+            if ammount > 10000:
+                raise exception
+            return ammount
+        except:
+            print('Please enter a valid value!')
+            continue
+
+
+coin_flipper(inputter())
